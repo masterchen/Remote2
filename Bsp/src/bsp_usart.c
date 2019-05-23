c76b612ba6960bb38600de34bb797cf5ee6a0462
masterchen <masterchen10000@gmail.com>
2019-05-23 16:22:26 +0800

    add uart2 support

--- Bsp/src/bsp_usart.c
+++ Bsp/src/bsp_usart.c
@@ -86,6 +86,14 @@ u8 USART_RX_BUF[USART_REC_LEN];     //接收缓冲,最大USART_REC_LEN个字节.
 //bit13~0，	接收到的有效字节数目
 u16 USART_RX_STA=0;       //接收状态标记	  
 
+u8 USART2_RX_BUF[USART_REC_LEN];     //接收缓冲,最大USART_REC_LEN个字节.
+//接收状态
+//bit15，	接收完成标志
+//bit14，	接收到0x0d
+//bit13~0，	接收到的有效字节数目
+u16 USART2_RX_STA=0;       //接收状态标记	
+
+
 //初始化IO 串口1 
 //bound:波特率
 void uart1_init(u32 bound){
@@ -165,3 +173,83 @@ void USART1_IRQHandler(void)                	//串口1中断服务程序
 } 
 #endif	
 
+
+//usart2初始化
+void USART2_Init(void)
+{
+	GPIO_InitTypeDef GPIO_InitStructure;
+	USART_InitTypeDef USART_InitStructure;
+	NVIC_InitTypeDef NVIC_InitStructure;
+	 
+	RCC_APB2PeriphClockCmd(RCC_APB1Periph_USART2|RCC_APB2Periph_GPIOA, ENABLE);	//使能USART2,GPIOA时钟
+
+	USART_DeInit(USART2);
+	
+	//GPIO端口模式设置PA.2 PA.3
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2; 
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;	//输出推免
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
+   
+ 
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3;//PA10
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;//浮空输入
+    GPIO_Init(GPIOA, &GPIO_InitStructure); 
+
+    //Usart2 NVIC 配置
+    NVIC_InitStructure.NVIC_IRQChannel = USART2_IRQn;
+		NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority=3 ;//抢占优先级3
+		NVIC_InitStructure.NVIC_IRQChannelSubPriority = 3;		//子优先级3
+		NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;			
+		NVIC_Init(&NVIC_InitStructure);	
+  
+    //USART参数初始化设置
+		USART_InitStructure.USART_BaudRate = 115200;//
+		USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+		USART_InitStructure.USART_StopBits = USART_StopBits_1;
+		USART_InitStructure.USART_Parity = USART_Parity_No;
+		USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+		USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;	
+
+    USART_Init(USART2, &USART_InitStructure); 
+    USART_ITConfig(USART2, USART_IT_RXNE, ENABLE);
+    //USART_ITConfig(USART2, USART_IT_IDLE, ENABLE);
+    USART_Cmd(USART2, ENABLE);                 
+}
+
+
+
+void USART2_IRQHandler(void)                	
+{
+	u8 Res;
+	#ifdef SYSTEM_SUPPORT_OS	 	
+	OSIntEnter();    
+	#endif 
+	if(USART_GetITStatus(USART2, USART_IT_RXNE) != RESET)  //接收中断(接收到的数据必须是0x0d 0x0a结尾)
+	{
+		Res =USART_ReceiveData(USART2);//(USART2->DR);	//读取接收到的数据
+		
+		if((USART2_RX_STA&0x8000)==0)//接收未完成
+		{
+			if(USART2_RX_STA&0x4000)//接收到了0x0d
+			{
+				if(Res!=0x0a)USART_RX_STA=0;//接收错误,重新开始
+				else USART2_RX_STA|=0x8000;	//接收完成了 
+			}
+			else //还没收到0X0D
+			{	
+				if(Res==0x0d)USART_RX_STA|=0x4000;
+				else
+					{
+					USART2_RX_BUF[USART2_RX_STA&0X3FFF]=Res ;
+						printf("%x",Res);
+					USART_RX_STA++;
+					if(USART2_RX_STA>(USART_REC_LEN-1))USART2_RX_STA=0;//接收数据错误,重新开始接收	  
+					}		 
+			}
+		}   		 
+   } 
+#ifdef SYSTEM_SUPPORT_OS	 
+	OSIntExit();  											 
+#endif
+}
